ece2874ac85b247efdb3caad87e1a9c797444d48
Matthieu Albert <matthieu_albert@groupevsc.com>
2020-02-21 17:19:22 +0100

    WIP : Regex

--- src/app.js
+++ src/app.js
@@ -7,11 +7,12 @@ const stream = T.stream('statuses/sample')
  
 stream.on('tweet', function (tweet) {
   let {id, text, truncated, retweeted_status: retweetedStatus, is_quote_status: isQuoteStatus, user: {id: userId, name, screen_name: screenName}, lang} = tweet
-  if(lang === 'fr' && !isQuoteStatus && !retweetedStatus) {
+  if(lang === 'fr' && !isQuoteStatus) {
     if(truncated) {
       text = tweet.extended_tweet.full_text
     }
     if(!text.includes('RT @')) {
+      console.log(/[^@](\w{3,}?)ine/gi.exec(text))
       console.log(id, text, userId, name, screenName)
     }
   }
