47b58544657a527b9d2de89ede5fb7442c4b7447
Matthieu Albert <matthieu_albert@groupevsc.com>
2020-02-21 10:21:38 +0100

    Fetching all tweets that can be RT and analyzed easily

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const Twit = require('twit')
+const config = require('../config/config.json')
+
+const T = new Twit(config)
+
+const stream = T.stream('statuses/sample')
+ 
+stream.on('tweet', function (tweet) {
+  let {id, text, truncated, retweeted_status: retweetedStatus, is_quote_status: isQuoteStatus, user: {id: userId, name, screen_name: screenName}, lang} = tweet
+  if(lang === 'fr' && !isQuoteStatus && !retweetedStatus) {
+    if(truncated) {
+      text = tweet.extended_tweet.full_text
+    }
+    if(!text.includes('RT @')) {
+      console.log(id, text, userId, name, screenName)
+    }
+  }
+  /*if(false) {
+    T.post('statuses/retweet/:id', { id: 'id' }, function (err, data, response) {
+      console.log(data)
+    })
+  }*/
+})
